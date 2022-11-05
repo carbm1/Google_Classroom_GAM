0ec245c4a5a8d502b5feed1cdd3728020686f039
Craig Millsap <craig@carbm1.com>
2022-11-05 05:41:54 -0500

    Show Teacher Classrooms

--- show_teacher_classrooms.ps1
+++ show_teacher_classrooms.ps1
@@ -1,5 +1,14 @@
 Param(
-    [Parameter(Mandatory=$True)][string]$TeacherEmail #The teacher of the classes you want to add the administrator to.
+    [Parameter(Mandatory=$True)][string]$TeacherEmail, #The teacher of the classes you want to add the administrator to.
+    [Parameter(Mandatory=$False)][switch]$ActiveOnly
 )
 
-& gam.exe print courses teacher $TeacherEmail state active fields id,name | ConvertFrom-CSV | Format-Table
+$classrooms = & gam.exe print courses teacher $TeacherEmail fields id,name,coursestate alias | ConvertFrom-CSV
+
+if ($ActiveOnly) {
+    $classrooms | Where-Object { $PSItem.courseState -eq "ACTIVE" } | Format-Table
+} else {
+    $classrooms | Format-Table
+}
+
+Write-Host "Dot Source this script for `$classrooms variable."
